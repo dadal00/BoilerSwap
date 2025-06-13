741da7c139f0697805aa5eb6b0b86f2310c27410
Dylan Adal <dylanadal.00@gmail.com>
2025-06-13 16:52:31 -0700

    fe: format

--- frontend/src/lib/models.ts
+++ frontend/src/lib/models.ts
@@ -1,37 +1,29 @@
 export type Account = {
-    email: string
-    password: string
+	email: string
+	password: string
 }
 
 export type Verification = {
-    id: string
-    code: string
+	id: string
+	code: string
 }
 
-export type ItemType = 
-  | "Furniture"
-  | "Electronics"
-  | "Books"
-  | "Kitchen"
-  | "Clothing";
+export type ItemType = 'Furniture' | 'Electronics' | 'Books' | 'Kitchen' | 'Clothing'
 
-export type Condition = 
-  | "Excellent"
-  | "Good"
-  | "Fair";
+export type Condition = 'Excellent' | 'Good' | 'Fair'
 
-export type Location = 
-  | "Cary Quad - East"
-  | "Wiley Hall"
-  | "Harrison Hall"
-  | "Earhart Hall"
-  | "Hillenbrand Hall"
-  | "Third Street Suites";
+export type Location =
+	| 'Cary Quad - East'
+	| 'Wiley Hall'
+	| 'Harrison Hall'
+	| 'Earhart Hall'
+	| 'Hillenbrand Hall'
+	| 'Third Street Suites'
 
 export type Product = {
-    item_type: ItemType
-    title: string
-    condition: Condition
-    location: Location
-    description?: string
+	item_type: ItemType
+	title: string
+	condition: Condition
+	location: Location
+	description?: string
 }
