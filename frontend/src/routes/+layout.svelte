2c43f94ad6e5a67f8739d8f1729bb8ed2bc66abe
shadEdkr <edfromkorea@gmail.com>
2025-06-18 01:16:45 -0400

    updated layout svelte component

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -1,18 +1,25 @@
 <script lang="ts">
-	import '../app.css'
-	import NavBar from '$lib/components/NavBar.svelte'
-	import { afterNavigate } from '$app/navigation'
+  import '../app.css'
+  import NavBar from '$lib/components/NavBar.svelte'
+  import Footer from '$lib/components/Footer.svelte'
+  import { afterNavigate } from '$app/navigation'
 
-	let { children } = $props()
+  let { children } = $props()
 
-	let currentRoute = $state('/')
-	afterNavigate((navigation) => {
-		currentRoute = navigation.to?.url.pathname as string
-	})
+  let currentRoute = $state('/')
+  afterNavigate((navigation) => {
+    currentRoute = navigation.to?.url.pathname ?? '/'
+  })
 </script>
 
 {#if !currentRoute.includes('/auth')}
-	<NavBar></NavBar>
+  <NavBar />
 {/if}
 
-{@render children()}
+<main>
+  {@render children()}
+</main>
+
+{#if !currentRoute.includes('/auth')}
+  <Footer />
+{/if}
