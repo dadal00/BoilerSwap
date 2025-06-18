7e2370daa60770d62d07d50339572d9bae567e1d
Dylan Adal <dylanadal.00@gmail.com>
2025-06-18 14:38:32 -0700

    fe: prettier

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -1,25 +1,25 @@
 <script lang="ts">
-  import '../app.css'
-  import NavBar from '$lib/components/NavBar.svelte'
-  import Footer from '$lib/components/Footer.svelte'
-  import { afterNavigate } from '$app/navigation'
+	import '../app.css'
+	import NavBar from '$lib/components/NavBar.svelte'
+	import Footer from '$lib/components/Footer.svelte'
+	import { afterNavigate } from '$app/navigation'
 
-  let { children } = $props()
+	let { children } = $props()
 
-  let currentRoute = $state('/')
-  afterNavigate((navigation) => {
-    currentRoute = navigation.to?.url.pathname ?? '/'
-  })
+	let currentRoute = $state('/')
+	afterNavigate((navigation) => {
+		currentRoute = navigation.to?.url.pathname ?? '/'
+	})
 </script>
 
 {#if !currentRoute.includes('/auth')}
-  <NavBar />
+	<NavBar />
 {/if}
 
 <main>
-  {@render children()}
+	{@render children()}
 </main>
 
 {#if !currentRoute.includes('/auth')}
-  <Footer />
+	<Footer />
 {/if}
